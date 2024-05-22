bd71c85047c0c276ac4e69eea28f324252bead34
Jacob Danell <jacob@emberlight.se>
2024-05-22 16:06:35 +0200

    test

--- scripts/update_sensors.py
+++ scripts/update_sensors.py
@@ -1,65 +1,123 @@
+import math
+import re
 import sys
 import json
 
 # Read input from stdin (provided via shell script)
-print("-------------------")
-print(sys.argv[1])
-print("------------------")
-exit(1)
-#VENDOR = sys.argv[1]
-#CAMERA = sys.argv[2]
-#TYPE = sys.argv[3]
+BODY = sys.argv[1]
 
-VENDOR = "Canon"
-CAMERA = "C300 Mark II"
-TYPE = """- Type: All
-  Focal Length (optional): 
-  Resolution: 4096 x 2160
-  mm: 24.6 x 13.8
-  inch: 0.968 x 0.543
-- Type: All2
-  Focal Length (optional): 
-  Resolution: 4096 x 2160
-  mm: 24.6 x 13.8
-  inch: 0.968 x 0.543"""
+
+def cleanup_block(text: str) -> tuple[str, str | None]:
+    lines = text.splitlines()
+    category = str(lines[0])
+    body = None
+    if str(lines[2]) != "_No response_":
+        body = "\n".join(lines[2:]).strip()
+    return (category, body)
+
+
+# Function to extract and convert the first number to a numeric type
+def extract_single_number(text: str) -> float:
+    number: list[str] = re.findall(r"\d*\.\d+|\d+", text.replace(",", "."))
+    return float(number[0])
+
+
+def extract_dual_numbers(text: str) -> list[float]:
+    numbers = re.findall(r"\d*\.\d+|\d+", text.replace(",", "."))
+    return [float(num) for num in numbers[:2]]
+
 
 # Read existing sensors.json file if it exists
 try:
-    with open('sensors.json', 'r') as f:
+    with open("sensors.json", "r") as f:
         sensors_data = json.load(f)
 except FileNotFoundError:
     sensors_data = {}
 
-# Ensure the necessary keys exist in the dictionary
-if VENDOR not in sensors_data:
-    sensors_data[VENDOR] = {}
-if CAMERA not in sensors_data[VENDOR]:
-    sensors_data[VENDOR][CAMERA] = {}
 
 resolutions = []
-for block in TYPE.strip().split('\n-'):
-    print("Block:", block)
-    resolution = {}
-    for line in block.strip().split('\\n'):
-        if ':' in line:
-            key, value = [x.strip() for x in line.split(':', 1)]
-            #print("key:", key, " ------ val:", value)
-            if key.lower() in ['resolution', 'mm', 'inch']:
-                width, height = [float(x) for x in value.split('x')]
-                resolution[key.lower()] = {'width': width, 'height': height}
-            elif key.lower() == 'type':
-                resolution['type'] = value
-            else:
-                resolution[key.lower().replace(' ', '_')] = value
-    resolutions.append(resolution)
+vendor = None
+camera = None
 
-#print(resolutions)
+blocks = BODY.strip().split("### Name", 1)
 
-# Update the dictionary with the new resolution data
-for resolution in resolutions:
-    type_name = resolution['type']
-    sensors_data[VENDOR][CAMERA][type_name] = resolution
+# Camera info
+for block in blocks[0].strip().split("### "):
+    category, body = cleanup_block(block)
+    if category == "Vendor":
+        vendor = body
+        if vendor not in sensors_data:
+            sensors_data[vendor] = {}
+
+    elif category == "Camera":
+        camera = body
+        if camera not in sensors_data[vendor]:
+            sensors_data[vendor][camera] = {}
+
+    elif category == "Additional Information" and body:
+        sensors_data[vendor][camera]["info"] = body
+
+# All resolution types
+for block in blocks[1:]:
+    mm = None
+    inches = None
+
+    # Add back the name category that got removed from the first split
+    block = f"### Name{block}"
+    res_type = block.strip().split("### ")
+
+    # Name
+    category, body = cleanup_block(res_type[0])
+    if body is None:
+        continue
+    res_name = body
+    sensors_data[vendor][camera][res_name] = {}
+
+    # Focal Length
+    category, body = cleanup_block(res_type[1])
+    if body:
+        sensors_data[vendor][camera][res_name]["focal_length"] = extract_single_number(
+            body
+        )
+
+    # Resolution
+    category, body = cleanup_block(res_type[2])
+    if body:
+        res = extract_dual_numbers(body)
+        sensors_data[vendor][camera][res_name]["resolution"] = {
+            "width": res[0],
+            "height": res[1],
+        }
+
+    # Sensor Size (mm)
+    category, body = cleanup_block(res_type[3])
+    if body:
+        mm = extract_dual_numbers(body)
+
+    # Sensor Size (inches)
+    category, body = cleanup_block(res_type[4])
+    if body:
+        inches = extract_dual_numbers(body)
+
+    if not mm and not inches:
+        raise AttributeError("You need at least one sensor size")
+    elif mm and not inches:
+        inches = [mm[0] / 25.4, mm[1] / 25.4]
+    elif not mm:
+        mm = [inches[0] * 25.4, inches[1] * 25.4]
+
+    sensors_data[vendor][camera][res_name]["mm"] = {
+        "width": mm[0],
+        "height": mm[1],
+        "diagonal": math.sqrt(mm[0] ** 2 + mm[1] ** 2),
+    }
+    sensors_data[vendor][camera][res_name]["inches"] = {
+        "width": inches[0],
+        "height": inches[1],
+        "diagonal": math.sqrt(inches[0] ** 2 + inches[1] ** 2),
+    }
+
 
 # Write the updated dictionary back to sensors.json
-with open('sensors.json', 'w') as f:
+with open("sensors.json", "w") as f:
     json.dump(sensors_data, f, indent=2)
